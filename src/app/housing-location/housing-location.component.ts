e99b8c339cef9f03ccfbcf992c19e668eb6250d0
Eduardo de Souza <desouzanovesete@gmail.com>
2024-02-21 17:41:57 -0300

    added housing-location component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { CardModule } from 'primeng/card';
+import { ButtonModule, } from 'primeng/button';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,CardModule,ButtonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }

9966677390997b9f6f8a985da36da53ed77f7a8b
Eduardo de Souza <desouzanovesete@gmail.com>
2024-02-25 23:51:39 -0300

    added http communication using json server, implemented forms and search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,26 @@ import { HousingLocation } from '../housinglocation';
 })
 
 export class HomeComponent {
+  filteredLocationList: HousingLocation[] = [];
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
-constructor() {
-  this.housingLocationList = this.housingService.getAllHousingLocations();
-}
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
 
 }
